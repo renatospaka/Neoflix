f06c9ddcae1b305f2abf3eb6181e41ea7793c6cd
Renato Spakauskas <renatospaka@gmail.com>
2020-08-19 00:21:12 -0300

    Week 1 - acessando o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Neo4jModule } from './neo4j/neo4j.module';
 
 @Module({
-  imports: [],
+  imports: [
+    Neo4jModule.forRoot({
+      scheme: 'neo4j',
+      host: 'localhost',
+      port: 7687,
+      username: 'neo4j',
+      password: 'neo',
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
