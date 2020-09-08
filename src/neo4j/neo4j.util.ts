b7022b2899b91631989cd3ac7d8206636ad1afbf
Renato Spakauskas <renatospaka@gmail.com>
2020-09-08 17:33:29 -0300

    Conexão com Neo4j apenas no Sandbox. Neo4j Desktop não funcionou

--- src/neo4j/neo4j.util.ts
+++ src/neo4j/neo4j.util.ts
@@ -2,11 +2,14 @@ import neo4j, { Driver } from 'neo4j-driver'
 import { Neo4jConfig } from '../neo4j-config.interface'
 
 export const createDriver = async (config: Neo4jConfig) => {
+    // const driver: Driver = neo4j.driver(
+    //     `${config.scheme}://${config.host}:${config.port}`,
+    //     neo4j.auth.basic(config.username, config.password)
+    // );
     const driver: Driver = neo4j.driver(
-        `${config.scheme}://${config.host}:${config.port}`,
-        neo4j.auth.basic(config.username, config.password)
+        'bolt://35.175.135.149:33002',
+        neo4j.auth.basic('neo4j', 'wrecks-detents-success')
     );
-
     await driver.verifyConnectivity()
 
     return driver;
