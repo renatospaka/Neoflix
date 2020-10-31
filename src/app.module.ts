fce05e162cb74e53a6965ee89d14b717c66fde82
Renato Spakauskas <renatospaka@gmail.com>
2020-10-31 13:17:35 -0300

    Connection to Sandbox, Docker or Server DONE.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Neo4jModule } from './neo4j/neo4j.module';
 
+console.log('vou chamar o Neo4j via neo4jdesktop')
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     Neo4jModule.forRoot({
-      scheme: 'neo4j',
-      host: 'localhost',
-      port: 7687,
-      username: 'neo4j',
-      password: 'letmein',
+      scheme: 'bolt',
+      host: process.env.NEO4J_HOST,
+      port: process.env.NEO4J_PORT,
+      username: process.env.NEO4J_USERNAME,
+      password: process.env.NEO4J_PASSWORD,
     })
   ],
   controllers: [AppController],
--- src/neo4j/neo4j.util.ts
+++ src/neo4j/neo4j.util.ts
@@ -2,14 +2,20 @@ import neo4j, { Driver } from 'neo4j-driver'
 import { Neo4jConfig } from '../neo4j-config.interface'
 
 export const createDriver = async (config: Neo4jConfig) => {
-    // const driver: Driver = neo4j.driver(
-    //     `${config.scheme}://${config.host}:${config.port}`,
-    //     neo4j.auth.basic(config.username, config.password)
-    // );
+    console.log('chamei Neo4j')
+    console.log(config.scheme)
+    console.log(config.host)
+    console.log(config.port)
+    console.log(config.username)
+    console.log(config.password );
     const driver: Driver = neo4j.driver(
-        'bolt://35.175.135.149:33002',
-        neo4j.auth.basic('neo4j', 'wrecks-detents-success')
+        `${config.scheme}://${config.host}:${config.port}`,
+        neo4j.auth.basic(config.username, config.password)
     );
+    // const driver: Driver = neo4j.driver(
+    //     'bolt://54.82.216.90:33519',
+    //     neo4j.auth.basic('neo4j', 'emergency-petroleum-term')
+    // );
     await driver.verifyConnectivity()
 
     return driver;
